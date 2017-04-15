1f00f35636f67b83916d99803b60f7660240cb5b
Valentin <val.dub03@gmail.com>
2017-04-15 10:36:59 +0800

    change borders

--- src/stabilizer.py
+++ src/stabilizer.py
@@ -24,20 +24,33 @@ def image_warping(cap2, fourcc, out, buffer_frame, max_number_frames, global_cor
             if border_type=="replace":
                 if shiftX > 0 :
                     # Left border #
-                    newFrame[0:frameHeight, 0:shiftX] = buffer_frame[0:frameHeight, 0:shiftX]
-                    newFrame[0:frameHeight, 0:shiftX+2] = cv2.blur(newFrame[0:frameHeight, 0:shiftX+2], (5,5))
+                    newFrame[0:frameHeight, 0:shiftX] = buffer_frame[0:frameHeight, 0:shiftX].copy()
                 elif shiftX < 0 :
                     # Right border #
-                    newFrame[0:frameHeight, frameWidth+shiftX:frameWidth] = buffer_frame[0:frameHeight, frameWidth+shiftX:frameWidth]
-                    newFrame[0:frameHeight, frameWidth+shiftX-2:frameWidth] = cv2.blur(newFrame[0:frameHeight, frameWidth+shiftX-2:frameWidth], (5,5))
+                    newFrame[0:frameHeight, frameWidth+shiftX:frameWidth] = buffer_frame[0:frameHeight, frameWidth+shiftX:frameWidth].copy()
                 if shiftY > 0 :
                     # Top border #
-                    newFrame[0:shiftY, 0:frameWidth] = buffer_frame[0:shiftY, 0:frameWidth]
-                    newFrame[0:shiftY+2, 0:frameWidth] = cv2.blur(newFrame[0:shiftY+2, 0:frameWidth], (5,5))
+                    newFrame[0:shiftY, 0:frameWidth] = buffer_frame[0:shiftY, 0:frameWidth].copy()
                 elif shiftY < 0 :
                     # Bottom border #
-                    newFrame[frameHeight+shiftY:frameHeight, 0:frameWidth] = buffer_frame[frameHeight+shiftY:frameHeight, 0:frameWidth]
-                    newFrame[frameHeight+shiftY-2:frameHeight, 0:frameWidth] = cv2.blur(newFrame[frameHeight+shiftY-2:frameHeight, 0:frameWidth], (5,5))
+                    newFrame[frameHeight+shiftY:frameHeight, 0:frameWidth] = buffer_frame[frameHeight+shiftY:frameHeight, 0:frameWidth].copy()
+
+                # We apply a blur on the new borders
+                blurredFrame = newFrame.copy()
+                # kernelX = np.float32([[1,1,1,1,1]])/5
+                # kernelY = np.float32([[0],[0.1],[0.25],[0.75],[1],[0.75],[0.25],[0.1],[0]])/3.2
+                if shiftX > 0 :
+                    # Left border #
+                    blurredFrame[0:frameHeight, 0:shiftX+2] = cv2.GaussianBlur(blurredFrame[0:frameHeight, 0:shiftX+2], (5,5), 0, 0, cv2.BORDER_REPLICATE)
+                elif shiftX < 0 :
+                    # Right border #
+                    blurredFrame[0:frameHeight, frameWidth+shiftX-2:frameWidth] = cv2.GaussianBlur(blurredFrame[0:frameHeight, frameWidth+shiftX-2:frameWidth], (5,5), 0, 0, cv2.BORDER_REPLICATE)
+                if shiftY > 0 :
+                    # Top border #
+                    blurredFrame[0:shiftY+2, 0:frameWidth] = cv2.GaussianBlur(blurredFrame[0:shiftY+2, 0:frameWidth], (5,5), 0, 0, cv2.BORDER_REPLICATE)
+                elif shiftY < 0 :
+                    # Bottom border #
+                    blurredFrame[frameHeight+shiftY-2:frameHeight, 0:frameWidth] = cv2.GaussianBlur(blurredFrame[frameHeight+shiftY-2:frameHeight, 0:frameWidth], (5,5), 0, 0, cv2.BORDER_REPLICATE)
 
             # Border are filled with white pixels #
             elif border_type=="white":
@@ -77,12 +90,28 @@ def image_warping(cap2, fourcc, out, buffer_frame, max_number_frames, global_cor
                     newFrame[frameHeight+shiftY:frameHeight, 0:frameWidth] = np.full((frameHeight, frameWidth, 3), 0, dtype=int)[frameHeight+shiftY:frameHeight, 0:frameWidth]
                     #newFrame[frameHeight+shiftY-2:frameHeight, 0:frameWidth] = cv2.blur(newFrame[frameHeight+shiftY-2:frameHeight, 0:frameWidth], (5,5))
 
-
-            # We apply a blur on the new borders
+            # if border_type=="test":
+            #     if shiftY < 0 :
+            #         # Bottom border
+            #         im1_gray = cv2.cvtColor(newFrame, cv2.COLOR_BGR2GRAY)
+            #         im2 = buffer_frame.copy()
+            #         im2[0:frameHeight+shiftY, 0:frameWidth] = np.full((frameHeight, frameWidth, 3), 0, dtype=int)[0:frameHeight+shiftY, 0:frameWidth]
+            #         im2_gray = cv2.cvtColor(im2, cv2.COLOR_BGR2GRAY)
+            #         warp_matrix = np.eye(2, 3, dtype=np.float32)
+            #         num_iter = 50
+            #         term_eps = 1e-10
+            #         criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, num_iter,  term_eps)
+            #         (cc, warp_matrix) = cv2.findTransformECC(im1_gray,im2_gray,warp_matrix, cv2.MOTION_AFFINE, criteria)
+            #         im2_aligned = cv2.warpAffine(im2, warp_matrix, (frameWidth,frameHeight),flags=cv2.INTER_LINEAR+cv2.WARP_INVERSE_MAP)
+            #         newFrame[frameHeight+shiftY:frameHeight, 0:frameWidth] = im2_aligned[frameHeight+shiftY:frameHeight, 0:frameWidth].copy()
 
             # We save the corrected frame #
-            out.write(newFrame)
-            buffer_frame=newFrame
+            if border_type=='replace':
+                out.write(blurredFrame)
+                buffer_frame=blurredFrame.copy()
+            else:
+                out.write(newFrame)
+                buffer_frame=newFrame
         else:
             # We release the second capture #
             cap2.release()
